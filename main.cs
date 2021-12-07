bc246a40c5f578a1f8e360c0a57c404027d37c7f
Edward Sionov <esionov@me.com>
2021-12-07 23:02:24 +0000

    throw exception.

--- main.cs
+++ main.cs
@@ -20,25 +20,21 @@ class Program
 
 	private static void CreateUsers(DatabaseContext context)
 	{
-		var user1 = new User { Id = Guid.NewGuid(), Age = 18 };
-		var user2 = new User { Id = Guid.NewGuid(), Age = 32 };
-		var user3 = new User { Id = Guid.NewGuid(), Age = 54 };
-		
-		var contact1 = new Contact { Id = user1.Id, Phone = "00001"};
-		var contact2 = new Contact { Id = user2.Id, Phone = "00002"};
-		var contact3 = new Contact { Id = user3.Id, Phone = "00003"};
+		foreach (var i in Enumerable.Range(0, 100))
+		{
+			var user = new User { Id = Guid.NewGuid(), Age = i };	
+			var contact = new Contact { Id = user.Id, Phone = $"{i}"};
 
-		context.Users.AddRange(user1, user2, user3);
-		context.Contacts.AddRange(contact1, contact2, contact3);
+			context.Users.Add(user);
+			context.Contacts.Add(contact);
+		}
+		
 		context.SaveChanges();
 	}
 
-	//надо найти список телефонов пользователей, у которых возраст больше 30
+	//нужно одним запросом выбрать 10 телефонов самых старших пользователей, у которых возраст более 30
 	private static Task<string[]> GetPhonesAsync(DatabaseContext context)
 	{		
-		return context.Users			
-			.Where(x => x.Age > 30)
-			.Select(x => x.Contact.Phone)
-			.ToArrayAsync();
+		throw new NotImplementedException();
 	}
 }
